9c3398370770ac8318c64b3acc07da7bea68426a
Maximilian Forstenhaeusler <Maxi@Maximilians-MacBook-Pro-5072.local>
2022-03-31 16:32:53 +0200

    updates Nico Kelling (speaker) text

--- components/03_sections/speakers.js
+++ components/03_sections/speakers.js
@@ -26,7 +26,7 @@ const data = {
     {
       "name": "Nico Kelling",
       "paragraph_highlighted": "Head of AI Center of Excellence at Infineon AG",
-      "paragraph": " He will share how a leading semiconductor company is using AI to products its producs and internal products.",
+      "paragraph": " He will share how a leading semiconductor company is using AI to produce its products and smartify its internal processes. And he will share insights on how Infineon is using future tech to contribute to society’s sustainability goals!",
       "linkedIn_link": "https://www.linkedin.com/in/nico-kelling-b686787/",
       "website_link": "https://www.infineon.com",
       "image": "/speakers/Nico_Kelling.jpg"
