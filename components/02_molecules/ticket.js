4b1018cff4cada6678df0eef74c11e73fcd351f5
mforstenhaeusler <Maxi@Maximilians-MacBook-Pro-5072.local>
2022-04-09 00:31:58 +0200

    disables application site

--- components/02_molecules/ticket.js
+++ components/02_molecules/ticket.js
@@ -5,18 +5,18 @@ export default function Ticket({position}){
   if(position == "right"){
     return <div style={{right: 0}}className={styles.TicketItem}>
       <div alt="Ticket" className={styles.Ticket}></div>
-      <Link href="/apply" passHref={true} >
+      <Link href="#" passHref={true} >
         <div className={styles.CtaButton}>
-          <div className={styles.Text}>Apply now</div>
+          <div className={styles.Text}>The application phase has ended!</div>
         </div>
       </Link>
     </div>
   }else{
     return <div className={styles.TicketItem}>
       <div alt="Ticket" className={styles.Ticket}></div>
-      <Link href="/apply" passHref={true} >
+      <Link href="#" passHref={true} >
         <div className={styles.CtaButton}>
-          <div className={styles.Text}>Apply now</div>
+          <div className={styles.Text}>The application phase has ended!</div>
         </div>
       </Link>
     </div>
