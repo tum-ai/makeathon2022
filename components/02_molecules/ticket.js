84b1e63af5e5d8f24f4bc938bfed85316836fb4b
ge93sut <ge93sut@mytum.de>
2022-08-28 18:12:51 +0200

    Final working version application

--- components/02_molecules/ticket.js
+++ components/02_molecules/ticket.js
@@ -1,13 +1,14 @@
 import styles from '../../styles/02_molecules/Ticket.module.css'
 import Link from 'next/link'
+// import "../../pages/application"
 
 export default function Ticket({position}){
   if(position == "right"){
     return <div style={{right: 0}}className={styles.TicketItem}>
       <div alt="Ticket" className={styles.Ticket}></div>
-      <Link href="#" passHref={true} >
+      <Link href="application" passHref={true} >
         <div className={styles.CtaButton}>
-          <div className={styles.Text}>Application starts soon!</div>
+          <div className={styles.Text}>Apply now!</div>
         </div>
       </Link>
     </div>
