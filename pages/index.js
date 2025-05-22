193096c6b29cda596056de422b3ac7030e1eb011
Zexin Gong <zexin.gong@tum.de>
2025-05-22 18:22:03 +0200

    fix: removed outdated information

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
         <About />
         <Roadmap />
         <Video />
-        <Banner />
+        {/* <Banner /> */}
         {/*<FullscreenPicture />
         <Element name="tracks">
           <Tracks />
